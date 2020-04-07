1e21d31f5a0caf42395d02f42ae693c7ec368270
hemanthsk20 <hemanthskmurthy@gmail.com>
2020-04-07 20:52:25 +0530

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,11 +11,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { HelloComponent } from './hello.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
-
+import { MatToolbarModule, MatIconModule, MatCardModule, MatButtonModule,MatProgressSpinnerModule} from '@angular/material';
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, AppRoutingModule,HttpClientModule ],
+  imports:[ BrowserModule, FormsModule, AppRoutingModule,HttpClientModule, MatToolbarModule, MatIconModule, MatButtonModule, MatCardModule ,MatProgressSpinnerModule],
   declarations: [ AppComponent, HelloComponent, HomeComponent, AboutComponent ],
   providers: [],
-  bootstrap:    [ AppComponent ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
